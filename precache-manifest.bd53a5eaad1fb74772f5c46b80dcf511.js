62e5a17859a196a223ebc8fdbcecb24a1155fd4e
anrtoineroulez <antoineroulezipad@gmail.com>
2020-10-19 16:22:03 +0200

    Updates

---
+++ precache-manifest.bd53a5eaad1fb74772f5c46b80dcf511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c3758a604eba3631970f6bc2623129",
+    "url": "/Monstres/index.html"
+  },
+  {
+    "revision": "e73d68f1a860972f0659",
+    "url": "/Monstres/static/css/main.f4221739.chunk.css"
+  },
+  {
+    "revision": "78f96b92dc0c6c461866",
+    "url": "/Monstres/static/js/2.ad8eb8ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monstres/static/js/2.ad8eb8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e73d68f1a860972f0659",
+    "url": "/Monstres/static/js/main.26658db0.chunk.js"
+  },
+  {
+    "revision": "7079b236122f0ef66439",
+    "url": "/Monstres/static/js/runtime-main.d17429ad.js"
+  }
+]);
